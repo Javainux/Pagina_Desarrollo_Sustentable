73631e3bd5318811f641dfee26273f0fad0498b5
Javainux <mateinge47@gmail.com>
2025-04-03 04:56:43 -0600

    Se agregó el archvio script.js para la rama lagos, su funcionalidad es que si el usuario da click en las imágenes se agranden a pantalla completa

---
+++ script.js
@@ -0,0 +1,15 @@
+const imagenes = document.querySelectorAll('.imgLagos img');
+const fullscreenDiv = document.createElement('div');
+fullscreenDiv.id = 'fullscreen';
+document.body.appendChild(fullscreenDiv);
+
+imagenes.forEach(img => {
+    img.addEventListener('click', () => {
+        fullscreenDiv.innerHTML = `<img src="${img.src}" alt="${img.alt}">`;
+        fullscreenDiv.style.display = 'flex';
+    });
+});
+
+fullscreenDiv.addEventListener('click', () => {
+    fullscreenDiv.style.display = 'none';
+});
