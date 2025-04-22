c3c24cde3e91182a7844c1eab982b9c7c0f3fa84
Javainux <mateinge47@gmail.com>
2025-04-21 23:10:24 -0600

    se arregló un error en el archivo 'transicion.js'

--- transicion.js
+++ transicion.js
@@ -1,13 +1,19 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const links = document.querySelectorAll("a");
-    links.forEach(link => {
-      link.addEventListener("click", (e) => {
-        e.preventDefault();
-        document.body.classList.add("fade-out");
-        setTimeout(() => {
-          window.location.href = link.href;
-        }, 500); // Tiempo que coincide con la duración de la transición CSS
-      });
+  const links = document.querySelectorAll("a");
+  
+  links.forEach(link => {
+    link.addEventListener("click", (e) => {
+      e.preventDefault();
+      document.body.classList.add("fade-out");
+
+      setTimeout(() => {
+        window.location.href = link.href;
+      }, 500); // Tiempo que coincide con la duración de la transición CSS
     });
   });
-  
+});
+
+// Asegura que la opacidad se restablezca al volver a la página con el botón de retroceso
+window.addEventListener("pageshow", () => {
+  document.body.classList.remove("fade-out");
+});
